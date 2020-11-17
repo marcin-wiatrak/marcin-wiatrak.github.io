f6f33adf1b94ffd96b0442d88ed1251e9d782b26
Marcin Wiatrak <marcin.wiatrak.95@gmail.com>
2020-11-17 22:38:11 +0100

    Rebuilding slides

--- wielosz_autodetailing/assets/main.js
+++ wielosz_autodetailing/assets/main.js
@@ -1,134 +1,37 @@
-const container = document.querySelector(".container");
-const leftArrow = document.querySelector(".arrow-left");
-const rightArrow = document.querySelector(".arrow-right");
-const navLiItems = document.querySelectorAll("nav ul li");
+const leftArrow = document.getElementsByClassName("arrow-left")[0];
+const rightArrow = document.getElementsByClassName("arrow-right")[0];
+const sectionsList = document.querySelector("[data-slide-id='2']");
 
-let slideNumber = 1;
+const firstSlide = document.getElementsByClassName("homepage")[0];
+firstSlide.classList.add("active-slide");
 
 
-const navigationTextArray = [
-    {
-        slideNumberFor: 1,
-        left: false,
-        leftArrowHTML: ``,
-        right: true,
-        rightArrowHTML: `<p class="navigation--smaller">Co możemy zrobić</p><p class="navigation--bigger">Dla Twojego auta</p>`,
-        specialClass: false,
-    },
-    {
-        slideNumberFor: 2,
-        left: true,
-        leftArrowHTML: `<p class="navigation--smaller">Wróć do</p><p class="navigation--bigger">Strony głównej</p>`,
-        right: true,
-        rightArrowHTML: `<p class="navigation--smaller">Zobacz nasze</p><p class="navigation--bigger">Realizacje</p>`,
-        specialClass: true,
-    },
-    {
-        slideNumberFor: 3,
-        left: true,
-        leftArrowHTML: `<p class="navigation--smaller">Co możemy zrobić</p><p class="navigation--bigger">Dla Twojego auta</p>`,
-        right: true,
-        rightArrowHTML: `<p class="navigation--bigger">Kontakt</p><p class="navigation--smaller">ze mną</p>`,
-        specialClass: false,
-    },
-    {
-        slideNumberFor: 4,
-        left: true,
-        leftArrowHTML: `<p class="navigation--smaller">Zobacz nasze</p><p class="navigation--bigger">Realizacje</p>`,
-        right: false,
-        rightArrowHTML: ``,
-        specialClass: true,
+const activeSlideGetDataId = () => {
+    let activeSlideDataId = 0;
+    const activeSlide = document.getElementsByClassName("active-slide")[0];
+    activeSlideDataId = activeSlide.dataset.slideId;
+    return activeSlideDataId;
+}
+
+const showNextSlide = (slideId, direction) => {
+    let nextSlideId = 0;
+    if(direction === 'right') {
+        nextSlideId = parseInt(slideId) + 1;
+    } else if(direction === 'left') {
+        nextSlideId = parseInt(slideId) - 1;
     }
-]
-
-let navigationArrowIndex = navigationTextArray.includes("Co możemy zrobić");
-console.log(navigationArrowIndex);
-
-const navigationItemChange = (slideNumberGet) => {
-    navLiItems.forEach(item => {
-        item.classList.remove("active");
-        item.classList.remove("active-negative");
-    })
-    if(slideNumberGet == 1 || slideNumberGet == 3) {
-        navLiItems[slideNumberGet-1].classList.add("active")
-    } else {
-        navLiItems[slideNumberGet-1].classList.add("active-negative")
-    }
+    const nextSlide = document.querySelector(`[data-slide-id='${nextSlideId}']`);
+    const thisSlide = document.querySelector(`[data-slide-id='${slideId}']`);
+    nextSlide.classList.add("active-slide")
+    thisSlide.classList.remove("active-slide");
 }
 
-const changeNavigationArrowText = (slideNumberGet) => {
-    
-    if(slideNumberGet) {
-        slideNumberGet--
-        if(navigationTextArray[slideNumberGet].right) {
-            rightArrow.style.visibility = "visible";
-            rightArrow.innerHTML =  navigationTextArray[slideNumberGet].rightArrowHTML;
-        } else {
-            rightArrow.style.visibility = "hidden";
-        }
-        if(navigationTextArray[slideNumberGet].left) {
-            leftArrow.style.visibility = "visible";
-            leftArrow.innerHTML =  navigationTextArray[slideNumberGet].leftArrowHTML;
-        } else {
-            leftArrow.style.visibility = "hidden";
-        }
-        if(navigationTextArray[slideNumberGet].specialClass) {
-            rightArrow.classList.add("color-negative");
-            leftArrow.classList.add("color-negative");
-        } else {
-            rightArrow.classList.remove("color-negative");
-            leftArrow.classList.remove("color-negative"); 
-        }
-    }
-}
-
-const getActualContainerWidth = () => {
-    const containerWidth = container.getBoundingClientRect().width;
-    return parseInt(containerWidth);
-} 
-
-const getActualContainerLeft = () => {
-    const containerLeftValue = getComputedStyle(container).left;
-    return parseInt(containerLeftValue);
-}
-
-const setLeftOnResize = () => {
-    const containerWidth = getActualContainerWidth();
-    const containerLeftNew = (containerWidth / 4) * slideNumber - (containerWidth / 4)
-    container.style.left = - containerLeftNew + "px";
-}
-const goToSlideNav = (slideNumberClick) => {
-    const containerWidth = getActualContainerWidth();
-    const containerLeftNew = (containerWidth / 4) * slideNumberClick - (containerWidth / 4)
-    container.style.left = - containerLeftNew + "px";
-
-    slideNumber = slideNumberClick
-    changeNavigationArrowText(slideNumberClick);
-    navigationItemChange(slideNumberClick)
-}
-
-leftArrow.addEventListener('click', function() {
-    container.style.left = getActualContainerLeft() + (getActualContainerWidth() / 4) + "px";
-    slideNumber--;
-    changeNavigationArrowText(slideNumber);
-    navigationItemChange(slideNumber)
-});
-rightArrow.addEventListener('click', function() {
-    container.style.left = getActualContainerLeft() - (getActualContainerWidth() / 4) + "px";
-    slideNumber++;
-    changeNavigationArrowText(slideNumber);
-    navigationItemChange(slideNumber)
-});
-
-navLiItems.forEach(liItem => {
-    liItem.addEventListener("click", function () {
-        const slideNumberClick = this.dataset.slideId;
-
-        goToSlideNav(slideNumberClick);
-    })
-
+rightArrow.addEventListener("click", function() {
+    let activeSlide = activeSlideGetDataId();
+    showNextSlide(activeSlide, 'right');
 })
 
-changeNavigationArrowText(slideNumber);
-navigationItemChange(slideNumber)
-window.onresize = setLeftOnResize;
+leftArrow.addEventListener("click", function() {
+    let activeSlide = activeSlideGetDataId();
+    showNextSlide(activeSlide, 'left');
+})
